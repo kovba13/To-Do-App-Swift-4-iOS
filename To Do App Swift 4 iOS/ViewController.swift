a5d8c025ef216dc1dedceaf03810e12ef570a6a4
Kostia Kovba <konstantinkovba@gmail.com>
2018-03-04 00:21:59 +0200

    Initial Commit

---
+++ To Do App Swift 4 iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  To Do App Swift 4 iOS
+//
+//  Created by Kostia Kovba on 04.03.18.
+//  Copyright © 2018 Kostia Kovba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
